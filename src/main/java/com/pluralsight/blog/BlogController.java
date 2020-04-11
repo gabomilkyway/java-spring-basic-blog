058872049ff2299ab90dd905f643f3247a360ed2
Gabriel Ravelo <gabomilkyway@gmail.com>
2020-04-11 04:31:32 -0400

    BlogController listPosts

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,27 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 @Controller
 public class BlogController {
+	
+	private PostRepository postRepository;
+	
+	public BlogController(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
 
 	@RequestMapping("/")
 	public String listPosts(ModelMap map) {
-		map.put("title", "Blog Post 1");
+		List<Post> posts = this.postRepository.getAllPosts();
+		map.put("posts", posts);
 		return "home";
 	}
 }
